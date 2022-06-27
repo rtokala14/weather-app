dd27902dfc8648c34f5b1f321ab12afbdd29fbfc
Rohit Tokala <rtokala@purdue.edu>
2022-06-27 19:21:39 +0400

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,38 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const isProduction = process.env.NODE_ENV === 'production';
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const isProduction = process.env.NODE_ENV === "production";
 
-const stylesHandler = 'style-loader';
+const stylesHandler = "style-loader";
 
-const config  = {
-  entry: './src/index.js',
+const config = {
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: "index.html",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(html)$/i,
+        loader: "html-loader",
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, 'css-loader', 'postcss-loader'],
+        use: [stylesHandler, "css-loader", "postcss-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
+        type: "asset",
       },
     ],
   },
@@ -36,9 +40,9 @@ const config  = {
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = 'production';
+    config.mode = "production";
   } else {
-    config.mode = 'development';
+    config.mode = "development";
   }
   return config;
 };
