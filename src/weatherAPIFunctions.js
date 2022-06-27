5baeacc471b69cdcc210e7ef489770b524d40387
Rohit Tokala <rtokala@purdue.edu>
2022-06-27 16:33:06 +0400

    update API to return forecast

--- src/weatherAPIFunctions.js
+++ src/weatherAPIFunctions.js
@@ -1,5 +1,5 @@
 async function getDataFromLocation(location) {
-  const requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=458d904b9b107250ccc645f8417f034d`;
+  const requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=458d904b9b107250ccc645f8417f034d`;
 
   const reponse = await fetch(requestURL, { mode: "cors" });
   const weatherData = await reponse.json();
