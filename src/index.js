a2c7be91e0e281b40dac9ada1928d9aa8d67ea94
Rohit Tokala <rtokala@purdue.edu>
2022-06-27 15:59:56 +0400

    Fetch data from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./style.css";
+import { getDataFromLocation } from "./weatherAPIFunctions";
+
+const searchForm = document.getElementById("enterWeatherLocation");
+
+searchForm.onsubmit = (e) => {
+  e.preventDefault();
+  searchWeather();
+};
+
+async function searchWeather() {
+  const searchBox = document.getElementById("cityName");
+  const weatherData = await getDataFromLocation(searchBox.value);
+  console.log(weatherData);
+  searchForm.reset();
+}
---
+++ src/weatherAPIFunctions.js
@@ -0,0 +1,10 @@
+async function getDataFromLocation(location) {
+  const requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=458d904b9b107250ccc645f8417f034d`;
+
+  const reponse = await fetch(requestURL, { mode: "cors" });
+  const weatherData = await reponse.json();
+
+  return weatherData;
+}
+
+export { getDataFromLocation };
